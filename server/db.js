7d074bb985572eb6524d439a393f304f96b056d5
alexander-effendy <88591194+alexander-effendy@users.noreply.github.com>
2024-05-28 17:44:54 +1000

    feat: oauth is working, see README.MD

--- server/db.js
+++ server/db.js
@@ -1,11 +1,34 @@
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   user: 'alexandereffendy',          
+//   host: 'localhost',                     
+//   database: 'boulder_hub',                 
+//   password: 'testdatabase',      
+//   port: 5432,                            
+// });
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: 'boulder_hub',
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
+
+// module.exports = pool;
+
 const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const pool = new Pool({
-  user: 'alexandereffendy',          
-  host: 'localhost',                     
-  database: 'boulder_hub',                 
-  password: 'testdatabase',      
-  port: 5432,                            
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 module.exports = pool;

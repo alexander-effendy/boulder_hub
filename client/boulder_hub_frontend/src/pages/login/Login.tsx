42d6568c872af6c68870b76536483744a2ccc9b1
Edward Lim <ed.lim199@gmail.com>
2024-05-29 14:40:32 +1000

    centered login items

--- client/boulder_hub_frontend/src/pages/login/Login.tsx
+++ client/boulder_hub_frontend/src/pages/login/Login.tsx
@@ -12,27 +12,45 @@ import { Label } from "@/components/ui/label"
 
 function Login() {
   return (
-    <Card className="w-full max-w-sm">
-      <CardHeader>
-        <CardTitle className="text-2xl">Login</CardTitle>
-        <CardDescription>
-          Enter your email below to login to your account.
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <div className="grid gap-2">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" required />
-        </div>
-        <div className="grid gap-2">
-          <Label htmlFor="password">Password</Label>
-          <Input id="password" type="password" required />
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button className="w-full">Sign in</Button>
-      </CardFooter>
-    </Card>
+    <div className="flex flex-col justify-center items-center gap-4 h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" type="email" placeholder="m@example.com" required />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" required />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full">Sign in</Button>
+        </CardFooter>
+      </Card>
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Login with Third Party Application</CardTitle>
+          <CardDescription>
+            Select one of the following:
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div className="grid gap-2">
+            <Button>Login with Google</Button>
+          </div>
+          <div className="grid gap-2">
+            <Button>Login with Facebook</Button>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
   )
 }
 
